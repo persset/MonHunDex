1c672676eddacfcc7e56cf168bf81428de112373
Jose Victor Persset <jpersset@gmail.com>
2022-12-22 13:43:26 -0300

    Setup monster model

--- MonHunDex/Controllers/MonsterController.cs
+++ MonHunDex/Controllers/MonsterController.cs
@@ -2,8 +2,10 @@
 
 namespace MonHunDex.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class MonsterController : ControllerBase
     {
-        [Route("api/[controller]")]
+        
     }
 }
--- MonHunDex/Models/Monster.cs
+++ MonHunDex/Models/Monster.cs
@@ -2,6 +2,10 @@
 {
     public class Monster
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Physiology { get; set; } = String.Empty;
+        public string Behavior { get; set; } = String.Empty;
     }
 }
