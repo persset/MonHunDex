34539cd762f9e9b94ab31791629c323705408772
Jose Victor Persset <jpersset@gmail.com>
2022-12-22 13:45:41 -0300

    create 'move' model

--- MonHunDex/Models/Move.cs
+++ MonHunDex/Models/Move.cs
@@ -1,11 +1,9 @@
 ﻿namespace MonHunDex.Models
 {
-    public class Monster
+    public class Move
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public string Physiology { get; set; } = string.Empty;
-        public string Behavior { get; set; } = string.Empty;
     }
 }
