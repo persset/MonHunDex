c084b0d7afc1ed641d3ab0469ecd224e7bded548
Jose Victor Persset <jpersset@gmail.com>
2022-12-27 11:52:45 -0300

    Create monster service interface

---
+++ MonHunDex/Services/Monster/IMonsterService.cs
@@ -0,0 +1,12 @@
+﻿using MonHunDex.Models;
+namespace MonHunDex.Services.Monster
+{
+    public interface IMonsterService
+    {
+        Task<List<Models.Monster>> GetAllMonster();
+        Task<Models.Monster?> GetSingleMonster();
+        Task<List<Models.Monster>> CreateMonster(Models.Monster monster);
+        Task<List<Models.Monster>> UpdateMonster(int id, Models.Monster monster);
+        Task<List<Models.Monster>> DeleteMonster(int id);
+    }
+}
