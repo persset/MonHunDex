a21e7871795d8faf2c40b2b63874214d62d69298
Jose Victor Persset <jpersset@gmail.com>
2022-12-27 11:26:09 -0300

    Add relationships between behavior and monster models/tables

---
+++ MonHunDex/Migrations/20221227142551_AddBehaviorTableRelationshipToMonsterTable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonHunDex.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBehaviorTableRelationshipToMonsterTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Behavior",
+                table: "SuperHeroes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BehaviorId",
+                table: "SuperHeroes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuperHeroes_BehaviorId",
+                table: "SuperHeroes",
+                column: "BehaviorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SuperHeroes_Behaviors_BehaviorId",
+                table: "SuperHeroes",
+                column: "BehaviorId",
+                principalTable: "Behaviors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SuperHeroes_Behaviors_BehaviorId",
+                table: "SuperHeroes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SuperHeroes_BehaviorId",
+                table: "SuperHeroes");
+
+            migrationBuilder.DropColumn(
+                name: "BehaviorId",
+                table: "SuperHeroes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Behavior",
+                table: "SuperHeroes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- MonHunDex/Models/Monster.cs
+++ MonHunDex/Models/Monster.cs
@@ -6,7 +6,8 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Physiology { get; set; } = string.Empty;
-        public string Behavior { get; set; } = string.Empty;
+        public int BehaviorId { get; set; }
+        public Behavior Behavior { get; set; }
         public ICollection<MonsterLocation> MonsterLocations { get; set; }
         public ICollection<MonsterElement> MonsterElements { get; set; }
         public ICollection<MonsterAilment> MonsterAilments { get; set; }
