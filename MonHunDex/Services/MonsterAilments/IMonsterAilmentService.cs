f8de2a386be4e11f97df2956d16c6e9c7924304f
Jose Victor Persset <jpersset@gmail.com>
2022-12-28 11:16:54 -0300

    Implement 'MonsterAilment' service

---
+++ MonHunDex/Services/MonsterAilments/IMonsterAilmentService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.MonsterAilments
+{
+    public interface IMonsterAilments
+    {
+        Task<List<Models.MonsterAilment>> GetAllMonsterAilments();
+        Task<Models.MonsterAilment?> GetSingleMonsterAilment(int id);
+        Task<Models.MonsterAilment> CreateMonsterAilment(Models.MonsterAilment monsterAilment);
+        Task<Models.MonsterAilment?> UpdateMonsterAilment(int id, Models.MonsterAilment request);
+        Task<Models.MonsterAilment?> DeleteMonsterAilment(int id);
+    }
+}
---
+++ MonHunDex/Services/MonsterAilments/MonsterAilmentService.cs
@@ -0,0 +1,66 @@
+﻿using MonHunDex.Data;
+using MonHunDex.Models;
+
+namespace MonHunDex.Services.MonsterAilments
+{
+    public class MonsterAilmentService : IMonsterAilmentService
+    {
+        private readonly DataContext dataContext;
+
+        public MonsterAilmentService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<MonsterAilment> CreateMonsterAilment(MonsterAilment monsterAilment)
+        {
+            dataContext.MonsterAilments.Add(monsterAilment);
+            await dataContext.SaveChangesAsync();
+
+            return monsterAilment;
+        }
+
+        public async Task<MonsterAilment?> DeleteMonsterAilment(int id)
+        {
+            var monsterAilment = await dataContext.MonsterAilments.FindAsync(id);
+
+            if (monsterAilment is null)
+                return null;
+
+            dataContext.MonsterAilments.Remove(monsterAilment);
+            await dataContext.SaveChangesAsync();
+
+            return monsterAilment;
+        }
+
+        public async Task<List<MonsterAilment>> GetAllMonsterAilments()
+        {
+            return await dataContext.MonsterAilments.ToListAsync();
+        }
+
+        public async Task<MonsterAilment?> GetSingleMonsterAilment(int id)
+        {
+            var monsterAilment = await dataContext.MonsterAilments.FindAsync(id);
+
+            if (monsterAilment is null)
+                return null;
+
+            return monsterAilment;
+        }
+
+        public async Task<MonsterAilment?> UpdateMonsterAilment(int id, MonsterAilment request)
+        {
+            var monsterAilment = await dataContext.MonsterAilments.FindAsync(id);
+
+            if (monsterAilment is null)
+                return null;
+
+            monsterAilment.MonsterId = request.MonsterId;
+            monsterAilment.AilmentId = request.AilmentId;
+
+            await dataContext.SaveChangesAsync();
+
+            return monsterAilment;
+        }
+    }
+}
