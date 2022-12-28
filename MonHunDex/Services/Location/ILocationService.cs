40bafcb6240582ae449d694571672ee905039b25
Jose Victor Persset <jpersset@gmail.com>
2022-12-28 10:44:05 -0300

    Implement 'Location' service

---
+++ MonHunDex/Services/Location/ILocationService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.Location
+{
+    public interface ILocationService
+    {
+        Task<List<Models.Location>> GetAllLocations();
+        Task<Models.Location?> GetSingleLocation(int id);
+        Task<Models.Location> CreateLocation(Models.Location location);
+        Task<Models.Location?> UpdateLocation(int id, Models.Location request);
+        Task<Models.Location?> DeleteLocation(int id);
+    }
+}
---
+++ MonHunDex/Services/Location/LocationService.cs
@@ -0,0 +1,66 @@
+﻿using MonHunDex.Data;
+
+namespace MonHunDex.Services.Location
+{
+    public class LocationService : ILocationService
+    {
+        private readonly DataContext dataContext;
+
+        public LocationService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.Location> CreateLocation(Models.Location location)
+        {
+            dataContext.Locations.Add(location);
+            await dataContext.SaveChangesAsync();
+
+            return location;
+        }
+
+        public async Task<Models.Location?> DeleteLocation(int id)
+        {
+            var location = await dataContext.Locations.FindAsync(id);
+
+            if (location is null)
+                return null;
+
+            dataContext.Locations.Remove(location);
+            await dataContext.SaveChangesAsync();
+
+            return location;
+        }
+
+        public async Task<List<Models.Location>> GetAllLocations()
+        {
+            return await dataContext.Locations.ToListAsync();
+        }
+
+        public async Task<Models.Location?> GetSingleLocation(int id)
+        {
+            var location = await dataContext.Locations.FindAsync(id);
+
+            if (location is null)
+                return null;
+
+            return location;
+        }
+
+        public async Task<Models.Location?> UpdateLocation(int id, Models.Location request)
+        {
+            var location = await dataContext.Locations.FindAsync(id);
+
+            if (location is null)
+                return null;
+
+            location.Name = request.Name;
+            location.Description = request.Description;
+            location.LocationTypeId = request.LocationTypeId;
+
+            await dataContext.SaveChangesAsync();
+
+            return location;
+        }
+    }
+}
