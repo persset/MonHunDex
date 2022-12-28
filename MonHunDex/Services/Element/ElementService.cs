4e3645d5808687c5f1b334aa019b90bd44110e18
Jose Victor Persset <jpersset@gmail.com>
2022-12-28 10:38:05 -0300

    Implement 'Element' service

---
+++ MonHunDex/Services/Element/ElementService.cs
@@ -0,0 +1,65 @@
+﻿using MonHunDex.Data;
+
+namespace MonHunDex.Services.Element
+{
+    public class ElementService : IElementService
+    {
+        private readonly DataContext dataContext;
+
+        public ElementService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.Element> CreateElement(Models.Element element)
+        {
+            dataContext.Elements.Add(element);
+            await dataContext.SaveChangesAsync();
+
+            return element;
+        }
+
+        public async Task<Models.Element?> DeleteElement(int id)
+        {
+            var element = await dataContext.Elements.FindAsync(id);
+
+            if (element is null)
+                return null;
+
+            dataContext.Elements.Remove(element);
+            await dataContext.SaveChangesAsync();
+
+            return element;
+        }
+
+        public async Task<List<Models.Element>> GetAllElements()
+        {
+            return await dataContext.Elements.ToListAsync();
+        }
+
+        public async Task<Models.Element?> GetSingleElement(int id)
+        {
+            var element = await dataContext.Elements.FindAsync(id);
+
+            if (element is null)
+                return null;
+
+            return element;
+        }
+
+        public async Task<Models.Element?> UpdateElement(int id, Models.Element request)
+        {
+            var element = await dataContext.Elements.FindAsync(id);
+
+            if (element is null)
+                return null;
+
+            element.Name = request.Name;
+            element.Description = request.Description;
+
+            await dataContext.SaveChangesAsync();
+
+            return element;
+        }
+    }
+}
---
+++ MonHunDex/Services/Element/IElementService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.Element
+{
+    public interface IElementService
+    {
+        Task<List<Models.Element>> GetAllElements();
+        Task<Models.Element?> GetSingleElement(int id);
+        Task<Models.Element> CreateElement(Models.Element element);
+        Task<Models.Element?> UpdateElement(int id, Models.Element request);
+        Task<Models.Element?> DeleteElement(int id);
+    }
+}
