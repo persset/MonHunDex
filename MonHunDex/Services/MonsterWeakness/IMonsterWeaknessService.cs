4339194684fe2cc01a6e9577f2703532d56efd84
Jose Victor Persset <jpersset@gmail.com>
2022-12-28 11:59:00 -0300

    Implement 'MonsterWeakness' service

---
+++ MonHunDex/Services/MonsterWeakness/IMonsterWeaknessService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.MonsterWeakness
+{
+    public interface IMonsterWeaknessService
+    {
+        Task<List<Models.MonsterWeakness>> GetAllMonsterWeaknesses();
+        Task<Models.MonsterWeakness?> GetSingleMonsterWeakness(int id);
+        Task<Models.MonsterWeakness> CreateMonsterWeakness(Models.MonsterWeakness monsterWeakness);
+        Task<Models.MonsterWeakness?> UpdateMonsterWeakness(int id, Models.MonsterWeakness request);
+        Task<Models.MonsterWeakness?> DeleteMonsterWeakness(int id);
+    }
+}
---
+++ MonHunDex/Services/MonsterWeakness/MonsterWeaknessService.cs
@@ -0,0 +1,66 @@
+﻿using MonHunDex.Data;
+using MonHunDex.Models;
+
+namespace MonHunDex.Services.MonsterWeakness
+{
+    public class MonsterWeaknessService : IMonsterWeaknessService
+    {
+        private readonly DataContext dataContext;
+
+        public MonsterWeaknessService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.MonsterWeakness> CreateMonsterWeakness(Models.MonsterWeakness monsterWeakness)
+        {
+            dataContext.MonsterWeaknesses.Add(monsterWeakness);
+            await dataContext.SaveChangesAsync();
+
+            return monsterWeakness;
+        }
+
+        public async Task<Models.MonsterWeakness?> DeleteMonsterWeakness(int id)
+        {
+            var monsterWeakness = await dataContext.MonsterWeaknesses.FindAsync(id);
+
+            if (monsterWeakness is null)
+                return null;
+
+            dataContext.MonsterWeaknesses.Remove(monsterWeakness);
+            await dataContext.SaveChangesAsync();
+
+            return monsterWeakness;
+        }
+
+        public async Task<List<Models.MonsterWeakness>> GetAllMonsterWeaknesses()
+        {
+            return await dataContext.MonsterWeaknesses.ToListAsync();
+        }
+
+        public async Task<Models.MonsterWeakness?> GetSingleMonsterWeakness(int id)
+        {
+            var monsterWeakness = await dataContext.MonsterWeaknesses.FindAsync(id);
+
+            if (monsterWeakness is null)
+                return null;
+
+            return monsterWeakness;
+        }
+
+        public async Task<Models.MonsterWeakness?> UpdateMonsterWeakness(int id, Models.MonsterWeakness request)
+        {
+            var monsterWeakness = await dataContext.MonsterWeaknesses.FindAsync(id);
+
+            if (monsterWeakness is null)
+                return null;
+
+            monsterWeakness.ElementId = request.ElementId;
+            monsterWeakness.MonsterId = request.MonsterId;
+
+            await dataContext.SaveChangesAsync();
+
+            return monsterWeakness;
+        }
+    }
+}
