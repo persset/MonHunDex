f84743249f4fa22f860a2d4a9905e905aded35e1
Jose Victor Persset <jpersset@gmail.com>
2022-12-28 10:56:32 -0300

    Implement 'MonsterClass' service

---
+++ MonHunDex/Services/MonsterClass/IMonsterClassService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.MonsterClass
+{
+    public interface IMonsterClassService
+    {
+        Task<List<Models.MonsterClass>> GetAllMonsterClasses();
+        Task<Models.MonsterClass?> GetSingleMonsterClass(int id);
+        Task<Models.MonsterClass> CreateMonsterClass(Models.MonsterClass monsterClass);
+        Task<Models.MonsterClass?> UpdateMonsterClass(int id, Models.MonsterClass request);
+        Task<Models.MonsterClass?> DeleteMonsterClass(int id);
+    }
+}
---
+++ MonHunDex/Services/MonsterClass/MonsterClassService.cs
@@ -0,0 +1,65 @@
+﻿using MonHunDex.Data;
+
+namespace MonHunDex.Services.MonsterClass
+{
+    public class MonsterClassService : IMonsterClassService
+    {
+        private readonly DataContext dataContext;
+
+        public MonsterClassService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.MonsterClass> CreateMonsterClass(Models.MonsterClass monsterClass)
+        {
+            dataContext.MonsterClasses.Add(monsterClass);
+            await dataContext.SaveChangesAsync();
+
+            return monsterClass;
+        }
+
+        public async Task<Models.MonsterClass?> DeleteMonsterClass(int id)
+        {
+            var monsterClass = await dataContext.MonsterClasses.FindAsync(id);
+
+            if (monsterClass is null)
+                return null;
+
+            dataContext.MonsterClasses.Remove(monsterClass);
+            await dataContext.SaveChangesAsync();
+
+            return monsterClass;
+        }
+
+        public async Task<List<Models.MonsterClass>> GetAllMonsterClasses()
+        {
+            return await dataContext.MonsterClasses.ToListAsync();
+        }
+
+        public async Task<Models.MonsterClass?> GetSingleMonsterClass(int id)
+        {
+            var monsterClass = await dataContext.MonsterClasses.FindAsync(id);
+
+            if (monsterClass is null)
+                return null;
+
+            return monsterClass;
+        }
+
+        public async Task<Models.MonsterClass?> UpdateMonsterClass(int id, Models.MonsterClass request)
+        {
+            var monsterClass = await dataContext.MonsterClasses.FindAsync(id);
+
+            if (monsterClass is null)
+                return null;
+
+            monsterClass.Name = request.Name;
+            monsterClass.Description = request.Description;
+
+            await dataContext.SaveChangesAsync();
+
+            return monsterClass;
+        }
+    }
+}
