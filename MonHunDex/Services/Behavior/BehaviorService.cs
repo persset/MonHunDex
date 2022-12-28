1c67166035a6c71897f4b0d23f188548b23d055b
Jose Victor Persset <jpersset@gmail.com>
2022-12-28 10:32:09 -0300

    Implement 'Behavior' service

---
+++ MonHunDex/Services/Behavior/BehaviorService.cs
@@ -0,0 +1,66 @@
+﻿using MonHunDex.Data;
+
+namespace MonHunDex.Services.Behavior
+{
+    public class BehaviorService : IBehaviorService
+    {
+        private readonly Data.DataContext dataContext;
+
+        public BehaviorService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.Behavior> CreateBehavior(Models.Behavior behavior)
+        {
+            dataContext.Behaviors.Add(behavior);
+
+            await dataContext.SaveChangesAsync();
+
+            return behavior;
+        }
+
+        public async Task<Models.Behavior?> DeleteBehavior(int id)
+        {
+            var behavior = await dataContext.Behaviors.FindAsync(id);
+
+            if (behavior is null)
+                return null;
+
+            dataContext.Behaviors.Remove(behavior);
+            await dataContext.SaveChangesAsync();
+
+            return behavior;
+        }
+
+        public async Task<List<Models.Behavior>> GetAllBehaviors()
+        {
+            return await dataContext.Behaviors.ToListAsync();
+        }
+
+        public async Task<Models.Behavior?> GetSingleBehavior(int id)
+        {
+            var behavior = await dataContext.Behaviors.FindAsync(id);
+
+            if (behavior is null)
+                return null;
+
+            return behavior;
+        }
+
+        public async Task<Models.Behavior?> UpdateBehavior(int id, Models.Behavior request)
+        {
+            var behavior = await dataContext.Behaviors.FindAsync(id);
+
+            if (behavior is null)
+                return null;
+
+            behavior.Name = request.Name;
+            behavior.Description = request.Description;
+
+            await dataContext.SaveChangesAsync();
+
+            return behavior;
+        }
+    }
+}
---
+++ MonHunDex/Services/Behavior/IBehaviorService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.Behavior
+{
+    public interface IBehaviorService
+    {
+        Task<List<Models.Behavior>> GetAllBehaviors();
+        Task<Models.Behavior?> GetSingleBehavior(int id);
+        Task<Models.Behavior> CreateBehavior(Models.Behavior behavior);
+        Task<Models.Behavior?> UpdateBehavior(int id, Models.Behavior request);
+        Task<Models.Behavior?> DeleteBehavior(int id);
+    }
+}
