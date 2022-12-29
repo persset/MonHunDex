fe80f34deddba8ae3485ac8ff95bb30461dc8d91
Jose Victor Persset <jpersset@gmail.com>
2022-12-29 10:31:05 -0300

    Implement 'Move' Service

---
+++ MonHunDex/Services/Move/IMoveService.cs
@@ -0,0 +1,11 @@
+﻿namespace MonHunDex.Services.Move
+{
+    public interface IMoveService
+    {
+        Task<List<Models.Move>> GetAllMoves();
+        Task<Models.Move?> GetSingleMove(int id);
+        Task<Models.Move> CreateMove(Models.Move move);
+        Task<Models.Move?> UpdateMove(int id, Models.Move request);
+        Task<Models.Move?> DeleteMove(int id);
+    }
+}
---
+++ MonHunDex/Services/Move/MoveService.cs
@@ -0,0 +1,65 @@
+﻿using MonHunDex.Data;
+
+namespace MonHunDex.Services.Move
+{
+    public class MoveService : IMoveService
+    {
+        private readonly DataContext dataContext;
+
+        public MoveService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.Move> CreateMove(Models.Move move)
+        {
+            dataContext.Moves.Add(move);
+            await dataContext.SaveChangesAsync();
+
+            return move;
+        }
+
+        public async Task<Models.Move?> DeleteMove(int id)
+        {
+            var move = await dataContext.Moves.FindAsync(id);
+
+            if (move is null)
+                return null;
+
+            dataContext.Moves.Remove(move);
+            await dataContext.SaveChangesAsync();
+
+            return move;
+        }
+
+        public async Task<List<Models.Move>> GetAllMoves()
+        {
+            return await dataContext.Moves.ToListAsync();
+        }
+
+        public async Task<Models.Move?> GetSingleMove(int id)
+        {
+            var move = await dataContext.Moves.FindAsync(id);
+
+            if (move is null)
+                return null;
+
+            return move;
+        }
+
+        public async Task<Models.Move?> UpdateMove(int id, Models.Move request)
+        {
+            var move = await dataContext.Moves.FindAsync(id);
+
+            if (move is null)
+                return null;
+
+            move.Name = request.Name;
+            move.Description = request.Description;
+
+            await dataContext.SaveChangesAsync();
+
+            return move;
+        }
+    }
+}
